afbc9d4bbeac6f66d06580e57b0317061d7686e0
arypte <wjs505@gmail.com>
2023-08-15 07:44:55 +0900

    [feature] 버튼추가
    div , ul , li

--- week1/study_1/src/app/page.tsx
+++ week1/study_1/src/app/page.tsx
@@ -1,7 +1,22 @@
 import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <main className="bg-red-100"> Home </main>;
+  return (
+    <main className="bg-red-100 py-24 px-12 min-h-screen">
+      <div className="font-semibold">Review English</div>
+      <ul>
+        <li>
+          <button>Day 1</button>
+        </li>
+        <li>
+          <button>Day 2</button>
+        </li>
+        <li>
+          <button>Day 3</button>
+        </li>
+      </ul>
+    </main>
+  );
 };
 
 export default Home;
