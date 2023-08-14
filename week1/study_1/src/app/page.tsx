e40ad5e2f58291613b0e2935838dd12711a2b113
arypte <wjs505@gmail.com>
2023-08-15 08:25:41 +0900

    [feature] data.json 생성

--- week1/study_1/src/app/page.tsx
+++ week1/study_1/src/app/page.tsx
@@ -1,14 +1,16 @@
 import DayCard from '@/components/DayCard';
 import { NextPage } from 'next';
+import data from '@/data.json';
 
 const Home: NextPage = () => {
   return (
     <main className="py-24 px-12 min-h-screen flex flex-col justify-start items-center">
       <div className="font-semibold">Review English</div>
       <ul className="mt-12 w-full grid grid-cols-4 gap-4">
-        <DayCard index={0} />
-        <DayCard index={1} />
-        <DayCard index={2} />
+        {data.map((v, i) => (
+          <DayCard key={i} index={i} />
+        ))}
+        {/* return ( ) 이 기본이지만 한줄이면 생략가능 */}
       </ul>
     </main>
   );
