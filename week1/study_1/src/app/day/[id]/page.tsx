75941b25d3ab703689859bb060be87bde179f139
arypte <wjs505@gmail.com>
2023-08-22 08:07:26 +0900

    [design] Day[id] 페이지 디자인 완성

--- week1/study_1/src/app/day/[id]/page.tsx
+++ week1/study_1/src/app/day/[id]/page.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import LeftArrow from '@/app/icons/LeftArrow';
 import RightArrow from '@/app/icons/RightArrow';
 import Speaker from '@/app/icons/Speaker';
 import { NextPage } from 'next';
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
 
 const Day: NextPage = () => {
   return (
@@ -15,23 +18,26 @@ const Day: NextPage = () => {
         </Link>
         <div className="font-semibold mb-24 bt-2">Day 1</div>
       </div>
-      <div className="bg-red-100 w-full">
-        <div>
-          <div>Working from home isn't for me. I always get distracted.</div>
-          <div>
-            <button>
+      <div className="w-full">
+        <div className="h-60">
+          <div className="border-black border-2 px-4 py-2">
+            Working from home isn't for me. I always get distracted.
+          </div>
+          <div classname="mt-2">
+            <button className="btn-style">
               <Speaker />
             </button>
           </div>
         </div>
-        <div>
-          <button>
+        <div className="mt-4 flex justify-between">
+          <button className="btn-style">
             <LeftArrow />
           </button>
-          <button>
-            <span>KOR</span>/<span>ENG</span>
+          <button className="btn-style">
+            <span className="mr-1">KOR</span>/
+            <span className="ml-1 font-semibold">ENG</span>
           </button>
-          <button>
+          <button className="btn-style">
             <RightArrow />
           </button>
         </div>
