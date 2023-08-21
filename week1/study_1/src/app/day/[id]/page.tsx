5f129250cf4f77fb2975144c73de88de9253cce1
arypte <wjs505@gmail.com>
2023-08-22 08:13:52 +0900

    [add] Day/[id] 페이지 component 분리
    useParms 사용

--- week1/study_1/src/app/day/[id]/page.tsx
+++ week1/study_1/src/app/day/[id]/page.tsx
@@ -3,11 +3,14 @@
 import LeftArrow from '@/app/icons/LeftArrow';
 import RightArrow from '@/app/icons/RightArrow';
 import Speaker from '@/app/icons/Speaker';
+import ReviewCard from '@/components/ReviewCard';
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
 
 const Day: NextPage = () => {
+  const { id } = useParams();
+  // 폴더 명과 일치해야함
   return (
     <main className="min-h-screen flex flex-col justify-start items-center py-24">
       <div className="relative w-full flex justify-center">
@@ -16,32 +19,9 @@ const Day: NextPage = () => {
             <button className="btn-style text-xs font-semibold">Back</button>
           </div>
         </Link>
-        <div className="font-semibold mb-24 bt-2">Day 1</div>
+        <div className="font-semibold mb-24 bt-2">Day {id}</div>
       </div>
-      <div className="w-full">
-        <div className="h-60">
-          <div className="border-black border-2 px-4 py-2">
-            Working from home isn't for me. I always get distracted.
-          </div>
-          <div classname="mt-2">
-            <button className="btn-style">
-              <Speaker />
-            </button>
-          </div>
-        </div>
-        <div className="mt-4 flex justify-between">
-          <button className="btn-style">
-            <LeftArrow />
-          </button>
-          <button className="btn-style">
-            <span className="mr-1">KOR</span>/
-            <span className="ml-1 font-semibold">ENG</span>
-          </button>
-          <button className="btn-style">
-            <RightArrow />
-          </button>
-        </div>
-      </div>
+      <ReviewCard />
     </main>
   );
 };
