a7f55a05ce07d77f8c4a7e9643c7bd4eb7b2991e
arypte <wjs505@gmail.com>
2023-08-22 07:54:10 +0900

    [design] Day[id] 페이지 디자인 - 하단부

--- week1/study_1/src/app/day/[id]/page.tsx
+++ week1/study_1/src/app/day/[id]/page.tsx
@@ -1,3 +1,6 @@
+import LeftArrow from '@/app/icons/LeftArrow';
+import RightArrow from '@/app/icons/RightArrow';
+import Speaker from '@/app/icons/Speaker';
 import { NextPage } from 'next';
 import Link from 'next/link';
 
@@ -5,12 +8,33 @@ const Day: NextPage = () => {
   return (
     <main className="min-h-screen flex flex-col justify-start items-center py-24">
       <div className="relative w-full flex justify-center">
-        <div className="absolute top-0 left-0">
-          <Link href="/">
+        <Link href="/">
+          <div className="absolute top-0 left-0">
             <button className="btn-style text-xs font-semibold">Back</button>
-          </Link>
-        </div>
+          </div>
+        </Link>
         <div className="font-semibold mb-24 bt-2">Day 1</div>
+      </div>
+      <div className="bg-red-100 w-full">
+        <div>
+          <div>Working from home isn't for me. I always get distracted.</div>
+          <div>
+            <button>
+              <Speaker />
+            </button>
+          </div>
+        </div>
+        <div>
+          <button>
+            <LeftArrow />
+          </button>
+          <button>
+            <span>KOR</span>/<span>ENG</span>
+          </button>
+          <button>
+            <RightArrow />
+          </button>
+        </div>
       </div>
     </main>
   );
