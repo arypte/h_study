5ac3bb4e0030981dad3639430d09b44f6770af42
arypte <wjs505@gmail.com>
2023-08-22 07:41:44 +0900

    [design] Day[id] 페이지 디자인
    버튼 css 추가

--- week1/study_1/src/app/day/[id]/page.tsx
+++ week1/study_1/src/app/day/[id]/page.tsx
@@ -1,15 +1,16 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 
 const Day: NextPage = () => {
   return (
-    <main>
-      <div>
-        <div>
+    <main className="min-h-screen flex flex-col justify-start items-center py-24">
+      <div className="relative w-full flex justify-center">
+        <div className="absolute top-0 left-0">
           <Link href="/">
-            <button>Back</button>
+            <button className="btn-style text-xs font-semibold">Back</button>
           </Link>
         </div>
-        <div>Day 1</div>
+        <div className="font-semibold mb-24 bt-2">Day 1</div>
       </div>
     </main>
   );
